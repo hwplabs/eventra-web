c88f4f1e8bdb52b27d5dc5aea861826763851abd
iamfeanyluch <ifeanyioluchi7@gmail.com>
2023-06-25 23:24:22 +0100

    Made changes to the filtertabs and the header button

--- src/components/FilterTabs/tablabels.ts
+++ src/components/FilterTabs/tablabels.ts
@@ -2,22 +2,23 @@ import { Filter } from "./types";
 
 export const tabLabels: { [key in Filter]: string } = {
   all: "All",
-  "for-you": "For You",
-  online: "Online",
+  virtual: "virtual",
   today: "Today",
   weekend: "Weekend",
-  "memorial-day": "Memorial Day",
-  "health-awareness": "Health Awareness",
-  free: "Free",
+  health_awareness: "Health Awareness",
   music: "Music",
-  drink: "Drink",
-  food: "Food",
-  "charity-causes": "Charity & Causes",
-  onlines: "Onlines",
-  todays: "Todays",
-  weekends: "Weekends",
-  "memorial-days": "Memorial Days",
-  "health-awarenesss": "Health Awarenesss",
-  frees: "Frees",
-  musics: "Musics",
+  food_and_drinks: "Food and Drinks",
+  exhibition: "exhibition",
+  politics: "politics",
+  talent_show: "talent_show",
+  sport: "sport",
+  movie_night: "Movie Night",
+  seminar: "seminars",
+  carnival: "carnival",
+  games: "games",
 };
+
+// [{
+//   id: 1,
+//   identifier: "exhibition"
+// }]
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, Link, Spacer, Image } from "@chakra-ui/react";
+import { Flex, Box, Button, Link, Spacer, Image } from "@chakra-ui/react";
 import _logo from "../../assets/logo/eventra.png";
 import DropdownMenu from "../DropdownButton/DropdownButton";
 
@@ -8,14 +8,16 @@ const Header: React.FC = () => {
     // Perform desired action based on the clicked item
   };
   return (
-    <Box mx={5}>
+    <Box mx={5} py={1}>
       <Flex>
         <Flex>
           <Image boxSize="50px" objectFit="cover" src={_logo} alt="logo" />
-          <Link href="/" fontSize="3xl" textDecoration="none">Eventra</Link>
+          <Link href="/" fontSize="3xl" textDecoration="none">
+            Eventra
+          </Link>
         </Flex>
         <Spacer />
-        <Flex>
+        {/* <Flex>
           <DropdownMenu
             buttonLabel="Organize"
             menuItems={[
@@ -34,15 +36,30 @@ const Header: React.FC = () => {
               { label: 'Contact Us', href: '/contact-us' },
             ]}
             onItemClick={handleItemClick}
-          />
-          <Flex mx={2}>
-            <Box mx={6} py={4}>
-              <Link fontWeight="500">Login</Link>
-            </Box>
-            <Box mx={6} py={4}>
-              <Link fontWeight="500">Signup</Link>
-            </Box>
-          </Flex>
+          /> */}
+        <Flex mx={2} alignItems="center" justifyContent="center">
+          <Box>
+            <Button
+              style={{
+                background: "#ECA500",
+                color: "#ffffff",
+                padding: "",
+                margin: "0 10px",
+              }}
+            >
+              Create Event
+            </Button>
+          </Box>
+          
+          <Box mx={6} py={4}>
+            <Link fontWeight="500" textDecoration="none">Login</Link>
+          </Box>
+          <Box mx={6} py={4}>
+            <Link fontWeight="500" textDecoration="none">Signup</Link>
+          </Box>
+          <Box mx={6} py={4}>
+            <Link fontWeight="500" textDecoration="none">Contact Us</Link>
+          </Box>
         </Flex>
       </Flex>
     </Box>
